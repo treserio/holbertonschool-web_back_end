c9e0f1d0d66aaf907e742f5a0badc21e566da70f
Tres Serio <treserio@hotmail.com>
2022-02-01 22:31:24 -0600

    10.1 refactor to map

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,9 +1,3 @@
 export default function appendToEachArrayValue(array, appendString) {
-  let i = 0;
-  for (var idx of array) {
-    array[i] = appendString + idx;
-    i += 1;
-  }
-
-  return array;
+  return array.map((idx) => appendString + idx);
 }
