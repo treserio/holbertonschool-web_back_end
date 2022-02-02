efddc3c0a3aabf029817c8b2be0b725e9eb040cf
Tres Serio <treserio@hotmail.com>
2022-02-02 15:00:54 -0600

    100.1 +comments, +keys variable

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,19 +1,24 @@
 export default function createIteratorObject(report) {
   const iterator = {
-    step: 0,
     dep: 0,
+    emp: 0,
     report: report.allEmployees,
     next() {
-      if (Object.keys(this.report)[this.dep]) {
-        this.step += 1;
-        if (this.report[Object.keys(this.report)[this.dep]][this.step - 1]) {
+      // store keys from the report
+      const keys = Object.keys(this.report);
+      // check that the allEmployee dictionary has a key
+      if (keys[this.dep]) {
+        // if this.emp exists, iterate the employee for the next next()
+        if (this.report[keys[this.dep]][this.emp]) {
+          this.emp += 1;
           return {
-            value: this.report[Object.keys(this.report)[this.dep]][this.step - 1],
+            // returning the the employee name at this.emp indx - 1 since we've moved passed it
+            value: this.report[keys[this.dep]][this.emp - 1],
             done: false,
           };
         }
         this.dep += 1;
-        this.step = 0;
+        this.emp = 0;
         return this.next();
       }
       return { value: null, done: true };
