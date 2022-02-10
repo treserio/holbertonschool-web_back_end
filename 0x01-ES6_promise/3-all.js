bce1d981f2c8a04fec906312d3573b3c533b974c
Tres Serio <treserio@hotmail.com>
2022-02-09 21:19:20 -0600

    3.02 changed console log str, 4 init

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -2,6 +2,6 @@ import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
   return Promise.all([uploadPhoto(), createUser()])
-    .then((val) => { console.log(val[0].body, val[1].firstName, val[1].lastName); })
+    .then((val) => { console.log(`${val[0].body} ${val[1].firstName} ${val[1].lastName}`); })
     .catch(() => { console.log('Signup system offline'); });
 }
