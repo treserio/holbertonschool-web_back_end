b1bc36538f2ed2545ec19cfbdb9e716fd050b28a
Tres Serio <treserio@hotmail.com>
2022-02-10 16:37:01 -0600

    9.2 eslinted

--- 0x01-ES6_promise/9-try.js
+++ 0x01-ES6_promise/9-try.js
@@ -1,7 +1,7 @@
 export default function guardrail(mathfunction) {
   try {
-    return [mathfunction(), 'Guardrail was processed']
+    return [mathfunction(), 'Guardrail was processed'];
   } catch (err) {
-    return [err.toString(), 'Guardrail was processed']
+    return [err.toString(), 'Guardrail was processed'];
   }
 }
