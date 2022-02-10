b20aca932aa71c031f25cc8639317ff5d3eb577e
Tres Serio <treserio@hotmail.com>
2022-02-09 20:57:11 -0600

    3 eslinted

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -3,5 +3,5 @@ import { uploadPhoto, createUser } from './utils';
 export default function handleProfileSignup() {
   Promise.all([uploadPhoto(), createUser()])
     .then((val) => { console.log(val[0].body, val[1].firstName, val[1].lastName); })
-    .catch(() => { console.log('Signup system offline'); })
+    .catch(() => { console.log('Signup system offline'); });
 }
