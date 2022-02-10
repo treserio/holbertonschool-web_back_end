a0d335893f9f3eb701d768d29092965bf3974ce5
Tres Serio <treserio@hotmail.com>
2022-02-10 13:34:59 -0600

    6.1 this question is bad

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,14 +1,11 @@
-import signUpUser from './4-user-promise'
-import uploadPhoto from './5-photo-reject'
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
 
-export default function handleProfileSignup(firstName, lastName, fileName) {
-  return Promise.resolve(Promise.all([signUpUser(firstName, lastName), uploadPhoto(fileName)])
-    .then((val) => {
-      console.log(val);
-      return { status: 'fulfilled', value: val }
-    })
-    .catch((val) => {
-      return { status: 'reject', value: val }
-    })
-  )
+export default async function handleProfileSignup(firstName, lastName, fileName) {
+  return [
+    await signUpUser(firstName, lastName)
+      .then((val) => ({ status: 'fulfilled', value: val })),
+    await uploadPhoto(fileName)
+      .catch((val) => ({ status: 'rejected', value: val.toString() })),
+  ];
 }
