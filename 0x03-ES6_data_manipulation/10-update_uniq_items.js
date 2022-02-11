c406768302ba5663636336f7bc1661a74c7d6473
Tres Serio <treserio@hotmail.com>
2022-02-10 21:10:19 -0600

    10.3 lower case p on error....

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -1,5 +1,5 @@
 export default function updateUniqueItems(map) {
-  if (!(map instanceof Map)) throw Error('Cannot Process');
+  if (!(map instanceof Map)) throw Error('Cannot process');
   for (const [k, v] of map) {
     if (v === 1) map.set(k, 100);
   }
