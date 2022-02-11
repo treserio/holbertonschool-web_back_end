f653730b4352bd008fcbbbf25ae5b48b28c8f8b5
Tres Serio <treserio@hotmail.com>
2022-02-10 18:31:10 -0600

    4 init

---
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -0,0 +1,9 @@
+export default function updateStudentGradeByCity(oList, city, newGrades) {
+  return oList
+    .filter((active) => active.location === city)
+    .map((st) => {
+      const g = newGrades.filter((g) => g.studentId === st.id);
+      const grade = g.length ? g[0].grade : 'N/A';
+      return { st, grade };
+    });
+}
