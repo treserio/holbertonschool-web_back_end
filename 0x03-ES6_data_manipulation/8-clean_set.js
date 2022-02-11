869a5f2896bc86bb4261736b9be883fca756635b
Tres Serio <treserio@hotmail.com>
2022-02-10 19:50:59 -0600

    8.1 try wrapper

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,10 +1,14 @@
 export default function cleanSet(set, startString) {
-  return Array.from(set)
-    .map((val) => {
-      if (startString && val.startsWith(startString)) {
-        return val.replace(startString, '');
-      }
-    })
-    .filter((val) => val != undefined)
-    .join('-');
+  try {
+    return Array.from(set)
+      .map((val) => {
+        if (startString && val.startsWith(startString)) {
+          return val.replace(startString, '');
+        }
+      })
+      .filter((val) => val)
+      .join('-');
+  } catch (e) {
+    return '';
+  }
 }
