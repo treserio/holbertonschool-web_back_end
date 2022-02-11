54cea97407f5bc854a75f2244f7215857d08777c
Tres Serio <treserio@hotmail.com>
2022-02-10 20:57:32 -0600

    10.1 returning new map

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -1,6 +1,12 @@
 export default function updateUniqueItems(inMap) {
   if (!(inMap instanceof Map)) throw Error('Cannot Process');
+  const oMap = new Map();
   inMap.forEach((v, k) => {
-    if (v === 1) inMap.set(k, 100);
+    if (v === 1) {
+      oMap.set(k, 100);
+    } else {
+      oMap.set(k, v);
+    }
   });
+  return oMap;
 }
