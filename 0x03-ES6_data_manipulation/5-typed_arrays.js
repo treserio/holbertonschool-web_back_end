3d97b184ea6756fb405c761c30529f44a620a0f4
Tres Serio <treserio@hotmail.com>
2022-02-10 19:12:15 -0600

    5.1 testing

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -1,8 +1,8 @@
 export default function createInt8TypedArray(length, position, value) {
   try {
-    const abuff = new Int8Array(length);
-    abuff[position] = value;
-    return abuff;
+    const buff = new ArrayBuffer(length);
+    new Int8Array(buff)[position] = value;
+    return buff;
   } catch (e) {
     throw Error('Position outside range');
   }
