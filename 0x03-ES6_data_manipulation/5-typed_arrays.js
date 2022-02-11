7924a60b7711f004c2140dace30f817b362ed6e7
Tres Serio <treserio@hotmail.com>
2022-02-10 19:16:08 -0600

    5.3 no mention of DataView

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -1,9 +1,6 @@
 export default function createInt8TypedArray(length, position, value) {
-  try {
-    const buff = new ArrayBuffer(length);
-    new Int8Array(buff)[position] = value;
-    return new DataView(buff, 0);
-  } catch (e) {
-    throw Error('Position outside range');
-  }
+  if (position > length) throw Error('Position outside range');
+  const buff = new ArrayBuffer(length);
+  new Int8Array(buff)[position] = value;
+  return new DataView(buff, 0);
 }
