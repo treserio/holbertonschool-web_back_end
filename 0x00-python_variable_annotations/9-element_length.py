69f81378e0f57583bea016a3707bfcef531605bc
Tres Serio <treserio@hotmail.com>
2022-06-01 22:55:23 -0500

    9 init

--- 0x00-python_variable_annotations/9-element_length.py
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -1,1 +1,11 @@
 #!/usr/bin/env python3
+''' Annotate the below function's parameters and return values with the
+appropriate types '''
+import typing
+
+
+def element_length(
+        lst: typing.Iterable[typing.Sequence]
+        ) -> typing.List[typing.Tuple[typing.Sequence, int]]:
+    ''' returns a list of tuples with their value & index from another list '''
+    return [(i, len(i)) for i in lst]
