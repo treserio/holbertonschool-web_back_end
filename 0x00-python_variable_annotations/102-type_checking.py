f2bd5ef60cdc83fad4f2e629098040eb5d4a4fd2
Tres Serio <treserio@hotmail.com>
2022-06-01 23:17:55 -0500

    102 init

--- 0x00-python_variable_annotations/102-type_checking.py
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -1,1 +1,20 @@
 #!/usr/bin/env python3
+''' Use mypy to validate the following piece of code and apply any necessary
+changes. '''
+import typing as typ
+
+
+def zoom_array(lst: typ.Tuple, factor: int = 2) -> typ.List:
+    ''' correct typing challenge '''
+    zoomed_in: typ.List = [
+        item for item in lst
+        for i in range(factor)
+    ]
+    return zoomed_in
+
+
+array = [12, 72, 91]
+
+zoom_2x = zoom_array(tuple(array))
+
+zoom_3x = zoom_array(tuple(array), int(3.0))
