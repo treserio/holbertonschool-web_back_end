aaa1ebdb071e50672cf07dffdc00579cfd165abb
Tres Serio <treserio@hotmail.com>
2022-06-01 22:24:42 -0500

    6.1 fixed Union type

--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -4,6 +4,6 @@ of integers and floats and returns their sum as a float. '''
 from typing import List, Union
 
 
-def sum_mixed_list(mxd_lst: List[int | float]) -> float:
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
     ''' return the fum of the mxd_lst '''
     return sum(mxd_lst)
