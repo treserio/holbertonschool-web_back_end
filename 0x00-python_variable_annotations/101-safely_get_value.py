9da5e4a26ed233264d0d46b51929e79ee61850c2
Tres Serio <treserio@hotmail.com>
2022-06-01 23:08:43 -0500

    101 init

--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -1,1 +1,16 @@
 #!/usr/bin/env python3
+''' Given the parameters and the return values, add type annotations to the
+function '''
+import typing as typ
+
+
+def safely_get_value(
+    dct: typ.Mapping,
+    key: typ.Any,
+    default: typ.Union[typ.TypeVar('T'), None] = None
+) -> typ.Union[typ.Any, typ.TypeVar('T')]:
+    ''' gotta look into exactly what this function is doing '''
+    if key in dct:
+        return dct[key]
+    else:
+        return default
