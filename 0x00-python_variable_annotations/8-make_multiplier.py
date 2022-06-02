9270a2db9f03fa8a8241a0e1755fcb7508b7ebde
Tres Serio <treserio@hotmail.com>
2022-06-01 22:43:30 -0500

    7-8 init

--- 0x00-python_variable_annotations/8-make_multiplier.py
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -1,1 +1,11 @@
 #!/usr/bin/env python3
+''' Write a type-annotated function make_multiplier that takes a float
+multiplier as argument and returns a function that multiplies a float by
+multiplier. '''
+import typing
+
+def make_multiplier(multiplier: float) -> typing.Callable[[float], float]:
+    def return_funk(n: float) -> float:
+        return n * multiplier
+    return return_funk
+
