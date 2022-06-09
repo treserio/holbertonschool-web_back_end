f15998697f7cfb5c2237d847cd6ad72b659cf552
Tres Serio <treserio@hotmail.com>
2022-06-08 22:31:30 -0500

    3-4 init

--- 0x00-caching/3-lru_cache.py
+++ 0x00-caching/3-lru_cache.py
@@ -1,1 +1,32 @@
 #!/usr/bin/env python3
+''' Create a class LRUCache that inherits from BaseCaching and is a caching
+system '''
+BasicCache = __import__('0-basic_cache').BasicCache
+
+
+class LRUCache(BasicCache):
+    ''' LRUCache, least recently used, Class '''
+    def __init__(self):
+        ''' init super(), iterator, and order dictionary '''
+        super().__init__()
+        self.lru_order = {}
+        self.lru = 0
+
+    def put(self, key, item):
+        ''' redefining puts method to use FIFO to maintain MAX_ITEMS '''
+        if len(self.cache_data) == self.MAX_ITEMS \
+                and key not in self.cache_data:
+            rmKey = min(self.lru_order, key=lambda k: self.lru_order[k])
+            del self.cache_data[rmKey]
+            print('DISCARD:', rmKey)
+            del self.lru_order[rmKey]
+        super().put(key, item)
+        self.lru_order[key] = self.lru
+        self.lru += 1
+
+    def get(self, key):
+        ''' the least recently USED, put & get? '''
+        if key and key in self.lru_order:
+            self.lru_order[key] = self.lru
+            self.lru += 1
+        return super().get(key)
--- 0x00-caching/4-mru_cache.py
+++ 0x00-caching/4-mru_cache.py
@@ -1,1 +1,32 @@
 #!/usr/bin/env python3
+''' Create a class MRUCache that inherits from BaseCaching and is a caching
+system '''
+BasicCache = __import__('0-basic_cache').BasicCache
+
+
+class MRUCache(BasicCache):
+    ''' MRUCache, least recently used, Class '''
+    def __init__(self):
+        ''' init super(), iterator, and order dictionary '''
+        super().__init__()
+        self.mru_order = {}
+        self.mru = 0
+
+    def put(self, key, item):
+        ''' redefining puts method to use FIFO to maintain MAX_ITEMS '''
+        if len(self.cache_data) == self.MAX_ITEMS \
+                and key not in self.cache_data:
+            rmKey = max(self.mru_order, key=lambda k: self.mru_order[k])
+            del self.cache_data[rmKey]
+            print('DISCARD:', rmKey)
+            del self.mru_order[rmKey]
+        super().put(key, item)
+        self.mru_order[key] = self.mru
+        self.mru += 1
+
+    def get(self, key):
+        ''' the least recently USED, put & get? '''
+        if key and key in self.mru_order:
+            self.mru_order[key] = self.mru
+            self.mru += 1
+        return super().get(key)
