60c1a7914548d03ddab824db7241b5725a0b2970
Tres Serio <treserio@hotmail.com>
2022-06-13 09:49:21 -0500

    0-1 init

--- 0x01-pagination/0-simple_helper_function.py
+++ 0x01-pagination/0-simple_helper_function.py
@@ -1,1 +1,11 @@
 #!/usr/bin/env python3
+''' Write a function named index_range that takes two integer arguments page
+and page_size. The function should return a tuple of size two containing a
+start index and an end index corresponding to the range of indexes to return
+in a list for those particular pagination parameters. '''
+import typing as typ
+
+
+def index_range(page: int, page_size: int) -> typ.Tuple[int, int]:
+	''' return a tuple of the start end ind '''
+	return (page_size * page) - page_size, page_size * page
