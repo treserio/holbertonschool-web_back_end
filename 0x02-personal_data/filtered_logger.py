d48c5a11f7670bcdf6a4e345f1661115ddf7c460
Tres Serio <treserio@hotmail.com>
2022-06-16 08:43:13 -0500

    1 init

--- 0x02-personal_data/filtered_logger.py
+++ 0x02-personal_data/filtered_logger.py
@@ -3,7 +3,46 @@
 obfuscated '''
 import re
 import typing as typ
+import logging
 
+
+class RedactingFormatter(logging.Formatter):
+    ''' Redacting Formatter class
+        '''
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: typ.List[str]):
+        ''' Instantiates the class using parent classes init method 
+        
+            Args:
+                self: all attributes and methods of the class
+                fields: list of keys to obfuscate the values of
+
+            Returns:
+                RedactingFormatter object
+        '''
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        ''' Redact self.fields values from record.msg
+
+            Args:
+                self: this object
+                record (LogRecord): the log containing the message to redact
+
+            Returns:
+                str: the redacted record.msg
+        '''
+        return filter_datum(
+            self.fields,
+            self.REDACTION,
+            super().format(record),
+            self.SEPARATOR
+        ).replace(self.SEPARATOR, f'{self.SEPARATOR} ')
 
 def filter_datum(
         fields: typ.List[str],
@@ -13,7 +52,7 @@ def filter_datum(
         ) -> str:
     ''' Obfuscate list of values from input string
 
-    Parameters:
+    Args:
         fields (list): list of keys to obfuscate values of from input string
         redaction (str): the string to use in place of the value
         message (str): the input string to obfuscate
