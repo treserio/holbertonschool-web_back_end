8a5d2fe7cc8fdaabe49065a3ad0fcfd9e143997d
Tres Serio <treserio@hotmail.com>
2022-06-28 14:24:39 -0500

    +shabang on db.py, str size on user.py

--- 0x05-user_authentication_service/user.py
+++ 0x05-user_authentication_service/user.py
@@ -12,7 +12,7 @@ class User(Base):
     __tablename__ = 'users'
     # strings may need to be capped at 250
     id = sql.Column(sql.Integer, primary_key=True)
-    email = sql.Column(sql.String, nullable=False)
-    hashed_password = sql.Column(sql.String, nullable=False)
-    session_id = sql.Column(sql.String)
-    reset_token = sql.Column(sql.String)
+    email = sql.Column(sql.String(250), nullable=False)
+    hashed_password = sql.Column(sql.String(250), nullable=False)
+    session_id = sql.Column(sql.String(250))
+    reset_token = sql.Column(sql.String(250))
