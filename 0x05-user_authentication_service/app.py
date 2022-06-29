35fee35846c77168ef8bd0e17b80000883e11e3a
Tres Serio <treserio@hotmail.com>
2022-06-28 22:54:12 -0500

    11 init

--- 0x05-user_authentication_service/app.py
+++ 0x05-user_authentication_service/app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 ''' temp Flask server
 '''
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, Response, abort
 from user import User
 from auth import Auth
 
@@ -40,6 +40,29 @@ def register_user() -> User:
         return jsonify({'message': 'values missing, email & password'}), 400
     except Exception:
         return jsonify({'message': 'email already registered'}), 400
+
+
+@app.route('/sessions', methods=['POST'], strict_slashes=False)
+def login_user() -> Response:
+    ''' Registering a user
+    JSON body:
+        - email
+        - password
+    Return:
+        - Session ID cookie in response with JSONed form
+        - 401 if login info is invalid
+    '''
+    if AUTH.valid_login(request.form['email'], request.form['password']):
+        res = jsonify({
+            "email": f"{request.form['email']}",
+            "message": "logged in"
+        })
+        res.set_cookie(
+            'session_id',
+            AUTH.create_session(request.form['email'])
+        )
+        return res
+    abort(401)
 
 
 if __name__ == "__main__":
