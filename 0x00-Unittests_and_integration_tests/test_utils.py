15d250948ea524bd8499e51a19e5a583962253bc
Tres Serio <treserio@hotmail.com>
2022-07-06 09:04:49 -0500

    1.1 corrected parameters

--- 0x00-Unittests_and_integration_tests/test_utils.py
+++ 0x00-Unittests_and_integration_tests/test_utils.py
@@ -9,25 +9,27 @@ from parameterized import parameterized, parameterized_class
 class TestAccessNestedMap(unittest.TestCase):
     ''' access_nested_map tester '''
     @parameterized.expand([
-        ({"a": 1}, ("a"), 1),
-        ({"a": {"b": 2}}, ("a"), {"b": 2}),
+        ({"a": 1}, ("a",), 1),
+        ({"a": {"b": 2}}, ("a",), {"b": 2}),
         ({"a": {"b": 2}}, ("a", "b"), 2)
     ])
     def test_access_nested_map(
         self,
         nested_map: dict,
         keys: tuple,
-        ret: typ.Union[int, dict]) -> None:
+        ret: typ.Union[int, dict]
+    ) -> None:
         ''' self explanatory test for access_nested_map '''
         self.assertEqual(access_nested_map(nested_map, keys), ret)
 
     @parameterized.expand([
-        ({}, ('a'))
+        ({}, ('a',)),
         ({'a': 1}, ('a', 'b'))
     ])
     def test_access_nested_map_exception(
         self,
         nested_map: dict,
-        keys: tuple) -> None:
+        keys: tuple
+    ) -> None:
         ''' test invalid keys for access_nested_map '''
         self.assertRaises(KeyError, access_nested_map, nested_map, keys)
