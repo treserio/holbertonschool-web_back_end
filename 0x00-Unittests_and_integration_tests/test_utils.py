1e63ca05109693ad5e432d79da32ab9b9af59266
Tres Serio <treserio@hotmail.com>
2022-07-06 09:25:37 -0500

    2 init

--- 0x00-Unittests_and_integration_tests/test_utils.py
+++ 0x00-Unittests_and_integration_tests/test_utils.py
@@ -1,17 +1,18 @@
 #!/usr/bin/env python3
 ''' Setting up unittests with mock and patch '''
-from utils import access_nested_map
-import unittest
+from utils import access_nested_map, get_json
+from unittest import TestCase as TC
+from unittest.mock import patch
 import typing as typ
 from parameterized import parameterized, parameterized_class
 
 
-class TestAccessNestedMap(unittest.TestCase):
+class TestAccessNestedMap(TC):
     ''' access_nested_map tester '''
     @parameterized.expand([
         ({"a": 1}, ("a",), 1),
         ({"a": {"b": 2}}, ("a",), {"b": 2}),
-        ({"a": {"b": 2}}, ("a", "b"), 2)
+        ({"a": {"b": 2}}, ("a", "b"), 2),
     ])
     def test_access_nested_map(
         self,
@@ -24,7 +25,7 @@ class TestAccessNestedMap(unittest.TestCase):
 
     @parameterized.expand([
         ({}, ('a',)),
-        ({'a': 1}, ('a', 'b'))
+        ({'a': 1}, ('a', 'b')),
     ])
     def test_access_nested_map_exception(
         self,
@@ -33,3 +34,16 @@ class TestAccessNestedMap(unittest.TestCase):
     ) -> None:
         ''' test invalid keys for access_nested_map '''
         self.assertRaises(KeyError, access_nested_map, nested_map, keys)
+
+
+class TestGetJson(TC):
+    ''' get_json tester '''
+    @parameterized.expand([
+        ("http://example.com", {"payload": True}),
+        ("http://holberton.io", {"payload": False}),
+    ])
+    def test_get_json(self, url: str, json: dict) -> None:
+        ''' test for get_json with mock.patch requests.get '''
+        with patch("requests.get") as m_g:
+            m_g.return_value.json.return_value = json
+            self.assertEqual(get_json(url), json)
