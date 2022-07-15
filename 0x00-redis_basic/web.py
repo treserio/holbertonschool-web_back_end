d56a65c83f1577678a2debc8c35f398fb57153f7
Tres Serio <treserio@hotmail.com>
2022-07-15 10:37:37 -0500

    5.3 expire in 10

--- 0x00-redis_basic/web.py
+++ 0x00-redis_basic/web.py
@@ -5,13 +5,9 @@ import redis
 
 
 def get_page(url: str) -> str:
-    ''' return HTML from url, and also'''
-    start = 0
-    if not start:
-        start += 1
-        cnt = 0
+    ''' return HTML from url, and also '''
     red = redis.Redis()
-    cnt += 1
-    red.set(f'count:{url}', cnt, 10)
+    red.incr(f'count:{url}')
+    red.expire(f'count:{url}', 10)
     res = requests.get(url)
     return res.content
