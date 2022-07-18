8029bfa6c188265443d3d720c6b537d1c2c89e33
Tres Serio <treserio@hotmail.com>
2022-07-18 14:54:56 -0500

    5.2 +DELIMITER change

--- 0x01-MySQL_Advanced/5-valid_email.sql
+++ 0x01-MySQL_Advanced/5-valid_email.sql
@@ -1,7 +1,11 @@
 -- create an update trigger on users
 -- reset valid_email to 0 if email changes
+DELIMITER $$
 
 CREATE TRIGGER reset_valid
 BEFORE UPDATE ON users FOR EACH ROW
-    IF NEW.id == OLD.id
-        SET valid_email = 0;
+BEGIN
+    IF NOT NEW.email = OLD.email THEN
+        SET NEW.valid_email = 0;
+    END IF;
+END$$
