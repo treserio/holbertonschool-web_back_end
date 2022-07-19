fb4797649f4c544e68e9531c1daa3c6a22976e07
Tres Serio <treserio@hotmail.com>
2022-07-18 20:19:36 -0500

    6.2 numerous syntax errors fixed

--- 0x01-MySQL_Advanced/6-bonus.sql
+++ 0x01-MySQL_Advanced/6-bonus.sql
@@ -1,20 +1,21 @@
--- create a procedure that updates a user's score for a procedure
-DELIMITER \O/
+-- create a procedure that updates a user's score for a project if it exists
+-- else add the project and update the user's score
+DELIMITER ~~
 
-
+DROP PROCEDURE IF EXISTS holberton.AddBonus;
 CREATE PROCEDURE AddBonus(
-    IN user_id int
-    IN project_name varchar(255)
+    IN user_id int,
+    IN project_name varchar(255),
     IN score int
 )
 BEGIN
-    IF NOT EXISTS (SELECT * projects WHERE name = project_name)
+    IF NOT EXISTS (SELECT * FROM projects WHERE name = project_name) THEN
         INSERT INTO projects (name) VALUES (project_name);
-        @proj_id = last_insert_id();
+        SET @proj_id = LAST_INSERT_ID();
     ELSE
-        @prod_id = SELECT id FROM projects WHERE name = project_name;
+        SELECT @proj_id := id FROM projects WHERE name = project_name;
     END IF;
 
     INSERT INTO corrections (user_id, project_id, score)
-    VALUES (user_id, @proj_id, score)
-END \O/
+    VALUES (user_id, @proj_id, score);
+END~~
