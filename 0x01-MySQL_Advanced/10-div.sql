ec58d664cfb3856603b9da90f8038db203c76729
Tres Serio <treserio@hotmail.com>
2022-07-18 22:48:28 -0500

    10 init

---
+++ 0x01-MySQL_Advanced/10-div.sql
@@ -0,0 +1,10 @@
+-- divide two numbers or returns 0 if the 2nd = 0
+DELIMITER ~~
+
+CREATE FUNCTION SafeDiv(a int, b int) RETURNS FLOAT
+BEGIN
+    IF b THEN
+        RETURN a / b;
+    END IF;
+    RETURN 0
+END~~
