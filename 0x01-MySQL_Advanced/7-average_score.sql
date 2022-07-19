769a0ae8f9e26e54dd74bd6a39e8f26d7fa66355
Tres Serio <treserio@hotmail.com>
2022-07-18 21:56:29 -0500

    7 init

---
+++ 0x01-MySQL_Advanced/7-average_score.sql
@@ -0,0 +1,16 @@
+-- create a procedure that updates a user's score for a project if it exists
+-- else add the project and update the user's score
+DELIMITER ~~
+
+DROP PROCEDURE IF EXISTS holberton.ComputeAverageScoreForUser;
+CREATE PROCEDURE ComputeAverageScoreForUser(
+    IN user_id int
+)
+BEGIN
+    SET @st_avg = (SELECT AVG(score) FROM corrections
+        WHERE user_id is user_id
+    );
+    UPDATE users
+    SET average_score = @st_avg
+    WHERE user_id = user_id;
+END~~
