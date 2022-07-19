1ea4eccbf63152b157aff3c6862e7c188c1760bb
Tres Serio <treserio@hotmail.com>
2022-07-18 22:20:07 -0500

    7.1 better

--- 0x01-MySQL_Advanced/7-average_score.sql
+++ 0x01-MySQL_Advanced/7-average_score.sql
@@ -3,14 +3,12 @@
 DELIMITER ~~
 
 DROP PROCEDURE IF EXISTS holberton.ComputeAverageScoreForUser;
-CREATE PROCEDURE ComputeAverageScoreForUser(
-    IN user_id int
-)
+CREATE PROCEDURE ComputeAverageScoreForUser(IN user_id int)
 BEGIN
     SET @st_avg = (SELECT AVG(score) FROM corrections
-        WHERE user_id is user_id
+        WHERE corrections.user_id = user_id
     );
     UPDATE users
     SET average_score = @st_avg
-    WHERE user_id = user_id;
+    WHERE id = user_id;
 END~~
