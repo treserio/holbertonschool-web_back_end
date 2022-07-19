ef66163660079626be5ae13cc2a2b34c067b5d95
Tres Serio <treserio@hotmail.com>
2022-07-18 22:54:14 -0500

    5.3 try without BEGIN

--- 0x01-MySQL_Advanced/5-valid_email.sql
+++ 0x01-MySQL_Advanced/5-valid_email.sql
@@ -4,8 +4,8 @@ DELIMITER $$
 
 CREATE TRIGGER reset_valid
 BEFORE UPDATE ON users FOR EACH ROW
-BEGIN
     IF NOT NEW.email = OLD.email THEN
         SET NEW.valid_email = 0;
     END IF;
-END$$
+
+DELIMITER ;
