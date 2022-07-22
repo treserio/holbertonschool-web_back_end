d012992dba223717b024e63d5c6d99c6f9c92162
Tres Serio <treserio@hotmail.com>
2022-07-22 14:24:41 -0500

    101.1 2 procedures

--- 0x01-MySQL_Advanced/101-average_weighted_score.sql
+++ 0x01-MySQL_Advanced/101-average_weighted_score.sql
@@ -1,6 +1,18 @@
 -- compute the average wheighted score for a student
 DELIMITER ~~
 
+CREATE PROCEDURE ComputeAverageWeightedScoreForUser(IN user_id INT)
+BEGIN
+    UPDATE users
+    SET average_score = (
+        SELECT (SUM(projects.weight * corrections.score) / SUM(projects.weight))
+        FROM corrections
+        INNER JOIN projects ON corrections.project_id = projects.id
+        WHERE corrections.user_id = user_id
+    )
+    WHERE users.id = user_id;
+END~~
+
 CREATE PROCEDURE ComputeAverageWeightedScoreForUsers()
 BEGIN
     FOR EACH ROW id IN users
