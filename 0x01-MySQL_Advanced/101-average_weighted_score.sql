8300ca9610a8dcdf0b930353a8f8288d4d70f4ad
Tres Serio <treserio@hotmail.com>
2022-07-26 13:14:15 -0500

    101.3 swapped to dbl join

--- 0x01-MySQL_Advanced/101-average_weighted_score.sql
+++ 0x01-MySQL_Advanced/101-average_weighted_score.sql
@@ -1,34 +1,16 @@
--- compute the average wheighted score for a student
+-- compute the average wheighted score for all students
 DELIMITER ~~
-
-DROP PROCEDURE IF EXISTS holberton.ComputeAverageWeightedScoreForUser;
-CREATE PROCEDURE ComputeAverageWeightedScoreForUser(IN user_id INT)
-BEGIN
-    UPDATE users
-    SET average_score = (
-        SELECT (SUM(projects.weight * corrections.score) / SUM(projects.weight))
-        FROM corrections
-        INNER JOIN projects ON corrections.project_id = projects.id
-        WHERE corrections.user_id = user_id
-    )
-    WHERE users.id = user_id;
-END~~
 
 DROP PROCEDURE IF EXISTS holberton.ComputeAverageWeightedScoreForUsers;
 CREATE PROCEDURE ComputeAverageWeightedScoreForUsers()
 BEGIN
-    SET @CustID = 0;
-    lp: WHILE TRUE DO
-        SET @CustID = (SELECT id FROM users
-            WHERE id > @CustID
-            ORDER BY id
-            LIMIT 1
-        );
-
-        IF FOUND_ROWS() = 0 THEN
-            LEAVE lp;
-        END IF;
-
-        CALL ComputeAverageWeightedScoreForUser(@CustID);
-    END WHILE;
+    UPDATE users
+    JOIN corrections ON corrections.user_id = users.id
+    JOIN projects ON projects.id = corrections.project_id
+    SET average_score = (
+        SELECT (SUM(projects.weight * corrections.score) / SUM(projects.weight))
+        FROM corrections
+        INNER JOIN projects ON corrections.project_id = projects.id
+        WHERE corrections.user_id = users.id
+    );
 END~~
