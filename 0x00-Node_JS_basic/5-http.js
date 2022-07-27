46f342fb2c5423ab46a8b9b60db385c021564249
Tres Serio <treserio@hotmail.com>
2022-07-27 03:22:05 -0500

    5 init

---
+++ 0x00-Node_JS_basic/5-http.js
@@ -0,0 +1,27 @@
+// a more complex http server
+const http = require('http');
+const countStudents = require('./3-read_file_async');
+
+const app = http.createServer((req, res) => {
+  if (req.url === '/') {
+    res.end('Hello Holberton School!');
+  } else if (req.url === '/students') {
+    res.write('This is the list of our students\n');
+
+    countStudents(process.argv[2])
+      .then((data) => {
+        res.write(`Number of students: ${data.students}\n`);
+        for (const [k, v] of Object.entries(data.fields)) {
+          res.write(`Number of students in ${k}: ${v.length}. List: ${v.join(', ')}`);
+          // add new line to every entry except the last key
+          if (k != Object.keys(data.fields)[Object.keys(data.fields).length - 1]) {
+            res.write('\n');
+          }
+        }
+        res.end();
+      })
+  }
+})
+  .listen(1245);
+
+module.exports = app;
