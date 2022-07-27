0d18c616f1d68d979cb59040edd92bedbd8b5814
Tres Serio <treserio@hotmail.com>
2022-07-27 02:27:24 -0500

    4.1 linter

--- 0x00-Node_JS_basic/4-http.js
+++ 0x00-Node_JS_basic/4-http.js
@@ -2,8 +2,8 @@
 const http = require('http');
 
 const app = http.createServer((req, res) => {
-    res.end('Hello Holberton School!');
-  })
+  res.end('Hello Holberton School!');
+})
   .listen(1245);
 
 module.exports = app;
