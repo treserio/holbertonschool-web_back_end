5e2eab37da7188fa715f44d7ce0b1d89c84bb715
Tres Serio <treserio@hotmail.com>
2022-07-26 23:46:36 -0500

    2.2 linter

--- 0x00-Node_JS_basic/2-read_file.js
+++ 0x00-Node_JS_basic/2-read_file.js
@@ -1,15 +1,16 @@
 const fs = require('fs');
 
 module.exports = function countStudents(file) {
+  let fullList = [];
   try {
-    var full_list = fs.readFileSync(file, 'utf8').split('\n').slice(1);
+    fullList = fs.readFileSync(file, 'utf8').split('\n').slice(1);
   } catch (e) {
     throw Error('Cannot load the database');
   }
-  students = full_list.filter(st => st != '');
+  const students = fullList.filter((st) => st !== '');
   // setup obj with our fields and their student lists
-  var fields = {};
-  students.forEach(st => {
+  const fields = {};
+  students.forEach((st) => {
     if (!fields[st.split(',')[3]]) fields[st.split(',')[3]] = [];
     fields[st.split(',')[3]].push(st.split(',')[0]);
   });
@@ -18,4 +19,4 @@ module.exports = function countStudents(file) {
   for (const [k, v] of Object.entries(fields)) {
     console.log(`Number of students in ${k}: ${v.length}. List: ${v.join(', ')}`);
   }
-}
+};
