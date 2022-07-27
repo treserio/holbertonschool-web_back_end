3a666f1916a34a33be063e368e587494221f36ab
Tres Serio <treserio@hotmail.com>
2022-07-26 23:28:51 -0500

    2 init

---
+++ 0x00-Node_JS_basic/2-read_file.js
@@ -0,0 +1,21 @@
+const fs = require('fs');
+
+function countStudents(file) {
+  try {
+    var full_list = fs.readFileSync(file, 'utf8').split('\n').slice(1);
+  } catch {
+    throw Error('Cannot load the database');
+  }
+  students = full_list.filter(st => st != '');
+  // setup obj with our fields and their student lists
+  var fields = {};
+  students.forEach(st => {
+    if (!fields[st.split(',')[3]]) fields[st.split(',')[3]] = [];
+    fields[st.split(',')[3]].push(st.split(',')[0]);
+  });
+  // print required messages
+  console.log(`Number of students: ${students.length}`);
+  for (const [k, v] of Object.entries(fields)) {
+    console.log(`Number of students in ${k}: ${v.length}. List: ${v.join(', ')}`);
+  }
+}
