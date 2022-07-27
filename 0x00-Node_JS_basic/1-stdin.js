57b8d177ca3f8355a7dee832f4751da2ea0b68c9
Tres Serio <treserio@hotmail.com>
2022-07-26 21:22:58 -0500

    1.1 linter

--- 0x00-Node_JS_basic/1-stdin.js
+++ 0x00-Node_JS_basic/1-stdin.js
@@ -1,13 +1,12 @@
 const readline = require('readline');
-const proc = require('process');
 
-input = proc.stdin;
-output = proc.stdout;
+const input = process.stdin;
+const output = process.stdout;
 
 console.log('Welcome to Holberton School, what is your name?');
-interface = readline.createInterface({input, output})
+const rlIn = readline.createInterface({ input, output })
   .on('line', (nm) => {
     console.log(`Your name is: ${nm}`);
-    interface.close();
+    rlIn.close();
   })
   .on('close', () => console.log('This important software is now closing'));
