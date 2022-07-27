89ca0b89786c3ec85e7dea5cc938226eb3bb08d0
Tres Serio <treserio@hotmail.com>
2022-07-26 22:18:29 -0500

    1.3 more linter

--- 0x00-Node_JS_basic/1-stdin.js
+++ 0x00-Node_JS_basic/1-stdin.js
@@ -3,6 +3,6 @@ console.log('Welcome to Holberton School, what is your name?');
 process.stdin
   .on('readable', () => {
     const nm = process.stdin.read();
-    nm && process.stdout.write(`Your name is: ${nm}`);
+    if (nm) { process.stdout.write(`Your name is: ${nm}`); }
   })
   .on('end', () => console.log('This important software is now closing'));
