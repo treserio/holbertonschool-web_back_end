f52a01c39901db9cde12aa0e68f679ac89be2696
Tres Serio <treserio@hotmail.com>
2022-07-27 20:32:40 -0500

    5.4 swap to single res instead of writes

--- 0x00-Node_JS_basic/5-http.js
+++ 0x00-Node_JS_basic/5-http.js
@@ -6,20 +6,20 @@ const app = http.createServer((req, res) => {
   if (req.url === '/') {
     res.end('Hello Holberton School!');
   } else if (req.url === '/students') {
-    res.write('This is the list of our students\n');
+    let output = 'This is the list of our students\n';
     countStudents(process.argv[2])
       .then((data) => {
-        res.write(`Number of students: ${data.total}\n`);
+        output += `Number of students: ${data.total}\n`;
         for (const [k, v] of Object.entries(data.fields)) {
-          res.write(`Number of students in ${k}: ${v.length}. List: ${v.join(', ')}`);
+          output += `Number of students in ${k}: ${v.length}. List: ${v.join(', ')}`;
           // add new line to every entry except the last key
           if (k !== Object.keys(data.fields)[Object.keys(data.fields).length - 1]) {
-            res.write('\n');
+            output += '\n';
           }
         }
-        res.end();
+        res.end(output);
       })
-      .catch((err) => res.end(err.message));
+      .catch((err) => res.end(output + err.message));
   }
 })
   .listen(1245);
