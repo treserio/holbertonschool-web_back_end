bdd92e9f79c7d1226bb6d56e9ed378bc14a0daba
Tres Serio <treserio@hotmail.com>
2022-08-01 22:04:24 -0500

    0x01-unittests init

---
+++ 0x01-unittests_in_js/0-calcul.test.js
@@ -0,0 +1,20 @@
+const assert = require('assert')
+const calculateNumber = require('./0-calcul')
+
+describe('calculateNumber', () => {
+	it('positives', () => {
+		assert.equal(calculateNumber(1, 3), 4);
+		assert.equal(calculateNumber(1, 3.7), 5);
+		assert.equal(calculateNumber(1.2, 3.7), 5);
+		assert.equal(calculateNumber(1.5, 3.7), 6);
+		assert.equal(calculateNumber(1.3, 0), 1);
+	});
+
+	it('negatives', () => {
+		assert.equal(calculateNumber(-1, 3), 2);
+		assert.equal(calculateNumber(-1, 3.7), 3);
+		assert.equal(calculateNumber(-1.2, -3.7), -5);
+		assert.equal(calculateNumber(1.5, -3.7), -2);
+		assert.equal(calculateNumber(-1.3, 0), -1);
+	});
+})
