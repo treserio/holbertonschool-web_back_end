51b809c77916303ab12523e8f9fb012a26be8e76
Tres Serio <treserio@hotmail.com>
2022-08-02 23:38:43 -0500

    4 init

---
+++ 0x01-unittests_in_js/4-payment.test.js
@@ -0,0 +1,28 @@
+const utils = require('./utils');
+const expect = require('chai').expect;
+const sinon = require('sinon');
+const sendPaymentRequestToApi = require('./3-payment');
+
+
+describe('sendPaymentRequestToApi', () => {
+  it('sendPaymentRequestToAPI = calculateNumber', () => {
+    const spiedFunction = sinon.spy(utils, 'calculateNumber');
+
+    sendPaymentRequestToApi(100, 20);
+
+    expect(spiedFunction.calledWith('SUM', 100, 20)).to.be.true;
+    spiedFunction.restore();
+  });
+
+  it('simulate a call to sendPaymentRequestToApi with stubs', () => {
+    const stub = sinon.stub(utils, 'calculateNumber').returns(10);
+    const spyFunction = sinon.spy(console, 'log');
+
+    sendPaymentRequestToApi(100, 20);
+
+    expect(spyFunction.calledWith('The total is: 10')).to.be.true;
+
+    stub.restore();
+    spyFunction.restore();
+  })
+});
